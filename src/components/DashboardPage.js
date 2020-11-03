e4bb1f6be9558d48e72abd45e4de0fc490a42bb3
gil rotem <gilxd931@gmail.com>
2020-11-03 11:45:02 +0200

    init commit

---
+++ src/components/DashboardPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DashboardPage = () => {
+    return (
+        <div>
+            Content of dashboard page
+        </div>
+    );
+};
+
+export default DashboardPage;
