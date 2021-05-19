be044c7b4a93fb8f71fdb4330edee9c9a1570e1a
gil rotem <gilxd931@gmail.com>
2021-05-20 00:13:50 +0300

    add 2 charts

--- src/components/ReportsPage.js
+++ src/components/ReportsPage.js
@@ -2,17 +2,61 @@ import React from 'react';
 import calculateTreatmentsPrices from '../selectors/CalculateTreatmentsPrices';
 import { connect } from 'react-redux';
 
+
+import PieChart, {
+    Series,
+    Label,
+    Connector,
+    Size,
+    Export
+} from 'devextreme-react/pie-chart';
+
+
 const ReportsPage = (props) => {
-    const pricesMap = calculateTreatmentsPrices(props.historyTreatments);
-    let totalPrice = 0;
-    for (const key in pricesMap) {
-        totalPrice += pricesMap[key];
-    }
+
+
+
+    const pricesList = calculateTreatmentsPrices(props.historyTreatments);
+
     return (
-        <div>
-            {
-                <p> סך כל ההכנסות: {totalPrice} שקלים</p>
-            }
+        <div className="form__edit-images">
+            <PieChart
+                id="pie"
+                dataSource={pricesList}
+                title="פילוח כמות טיפולים"
+            >
+                <Series
+                    argumentField="treat"
+                    valueField="count"
+                >
+                    <Label visible={true}>
+                        <Connector visible={true} width={1} />
+                    </Label>
+                </Series>
+
+                <Size width={700} />
+                <Export enabled={true} />
+            </PieChart>
+            <PieChart
+                id="pie"
+                dataSource={pricesList}
+                palette="Bright"
+                title="פילוח רווחי טיפולים"
+            >
+                <Series
+                    argumentField="treat"
+                    valueField="price"
+                >
+                    <Label visible={true}>
+                        <Connector visible={true} width={1} />
+                    </Label>
+                </Series>
+
+                <Size width={700} />
+                <Export enabled={true} />
+            </PieChart>
+
+
         </div >
     );
 };
