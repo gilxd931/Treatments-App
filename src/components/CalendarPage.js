4f66fdf9411351e66e5bc3a2653e2bde9bd72afa
gil rotem <gilxd931@gmail.com>
2021-05-22 14:44:31 +0300

    make big calendar work

--- src/components/CalendarPage.js
+++ src/components/CalendarPage.js
@@ -1,37 +1,62 @@
 import React from 'react';
 import BigCalendar from 'react-big-calendar'
 import moment from 'moment'
-require('style-loader!css-loader!react-big-calendar/lib/css/react-big-calendar.css')
+import { connect } from 'react-redux';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const localizer = BigCalendar.momentLocalizer(moment);
 
+const getEvents = (allTreatments) => {
+    let id = 0;
+    let parsedAllTreatments = [];
+    allTreatments.map((treatment) => (
 
+        parsedAllTreatments.push(
+            {
+                id: id++,
+                title: `${treatment.selected} ל${treatment.clientName}`,
+                start: new Date(treatment.date),
+                end: new Date(treatment.date + 60 * 60 * 1000),
+            }
+        )
+    ))
+    console.log(parsedAllTreatments)
+    return parsedAllTreatments;
+};
 
-const TreatmentsPage = () => {
+const TreatmentsPage = (props) => {
     return (
         <div>
             <BigCalendar
-                style={{ height: '420px' }}
-                events={[{
-                    id: 0,
-                    title: "All Day Event very long title",
-                    allDay: true,
-                    start: new Date(2020, 11, 11),
-                    end: new Date(2020, 11, 11)
-                },
-                {
-                    id: 5,
-                    title: "Conference",
-                    start: new Date(2020, 11, 22),
-                    end: new Date(2020, 11, 22),
-                    desc: "Big conference for important people"
-                }]}
+                style={{ height: '750px' }}
+                events={getEvents(props.historyTreatments.concat(props.futureTreatments))}
                 localizer={localizer}
                 startAccessor="start"
                 endAccessor="end"
+                culture='he-HE'
+                messages={{
+                    next: "הבא",
+                    previous: "הקודם",
+                    today: "היום",
+                    month: "חודש",
+                    week: "שבוע",
+                    day: "יום",
+                    agenda: "אג'נדה",
+                    event: "טיפולים",
+                    time: "זמן",
+                    date: "תאריך",
+                }}
             />
         </div>
     );
 };
 
-export default TreatmentsPage;
+const mapStateToProps = (state) => {
+    return {
+        futureTreatments: state.futureTreatments,
+        historyTreatments: state.historyTreatments
+    };
+};
+
+export default connect(mapStateToProps)(TreatmentsPage);
+
